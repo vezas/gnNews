a9afbb8cb30b87450a2d22fdb4f38f7d36fecc80
Radek Wrona <radoslaw.wrona1@gmail.com>
2023-03-30 21:41:19 +0200

    fix: add base to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), svgr()]
+  plugins: [tsconfigPaths(), svgr()],
+  base: '/gnNews/'
 });
